31f4371cad035ff5fdc2b1912430138bfc7b2969
Bundy-Mundi <bennie.ben095@gmail.com>
2021-10-14 13:18:07 -0700

    Re-did MaxStack

--- assignment/Project 1/MaxStack.py
+++ assignment/Project 1/MaxStack.py
@@ -1,23 +1,20 @@
 from Interfaces import Stack
 import SLLStack
-import copy
-
 
 class MaxStack(Stack) :
     def __init__(self):
         self.stack = SLLStack.SLLStack()
+        self.max_stack = SLLStack.SLLStack()
         
     def max(self) ->object:
-        s = copy.deepcopy(self.stack)
-        maxNum = 0
-        while s.size(): 
-            maxNum = max(maxNum, s.pop())
-        return maxNum
+        return self.max_stack.top()
     
     def push(self, x : object) : 
         self.stack.push(x)
+        self.max_stack.push(max(x, self.max_stack.top()))
 
     def pop(self) -> object:
+        self.max_stack.pop()
         return self.stack.pop()
 
     def size(self) -> int:
