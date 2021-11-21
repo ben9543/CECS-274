c79cb1112e4ffaf89e63f54e7d1c512b6a06f963
Bundy-Mundi <bennie.ben095@gmail.com>
2021-11-21 12:54:25 -0800

    BinarySearchTreeWithDup

--- assignment/Project 1/BinarySearchTreeWithDuplication.py
+++ assignment/Project 1/BinarySearchTreeWithDuplication.py
@@ -12,22 +12,28 @@ class BinarySearchTreeWithDuplication(Set):
         return self.n 
 
     def find(self, x: object) -> object:
-        pass
+        return self.binaryTree.find_eq(x)
 
     def add(self, key : object, value : object) -> bool:
-        pass    
-    
+        l = self.find(key)
+        if(l is not None):
+            #   Add new node to the list
+            l.v.append(value)
+        else:
+            #   Create new list
+            newList = [value]
+            self.binaryTree.add(key, newList)
+        self.n+=1
+        
     def remove(self, x : object) -> bool:
         pass
-
-'''         
-q = BinarySearchTreeWithKeyDuplication()
+        # return self.binaryTree.remove(x)
+     
+q = BinarySearchTreeWithDuplication()
 q.add(1, "a")
 q.add(1, "b")
 q.add(2, "c")
 q.add(3, "d")
 q.add(3, "e")
-find(1)
-find(2)
-find(3)
-'''
+
+print(q.find(1).v, q.find(2).v, q.find(3).v)
